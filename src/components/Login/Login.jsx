e82490dfaad71d3c2acb7fbf0dc4121ba9108c00
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-09-16 17:23:42 -0300

    commit practica obligatoria 3

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
 
-
 const Login = () => {
+  const [username, setUsername] = useState("");
 
-    const [username, setUsername] = useState("");
+  const setUsernameHandler = (event) => {
+    const updatedUsername = event.target.value;
+    setUsername(updatedUsername);
+    updatedUsername.toLowerCase().includes("o")
+      ? alert("Por favor, ¡Nombres de usuario sin letra 'o'!")
+      : 0;
+  };
 
-    const setUsernameHandler = (event) => {
-        setUsername(event.target.value);
-        username.toLowerCase().includes("o") ? alert("Por favor, ¡Nombres de usuario sin letra 'o'!") : 0;
-    }
+  const submitUsernameHandler = (event) => {
+    event.preventDefault();
+    username.toLowerCase().includes("o") || username.trim() == ""
+      ? alert("Usuario inválido para registrarse.")
+      : alert("Usuario registrado correctamente.");
 
-    const submitUsernameHandler = (event) => {
-        event.preventDefault();
-        username.toLowerCase().includes("o") || username.trim() == ""
-        ? alert("Usuario inválido para registrarse.")
-        : alert("Usuario registrado correctamente.");
+    setUsername("");
+  };
 
-        setUsername("");
-    }
-
-    return(
-        <>
-            <label id="username">Nombre de usuario: </label>
-            <input id="username" type="text" onChange={setUsernameHandler} value={username}></input>
-            <button id="register-btn" type="submit" onClick={submitUsernameHandler}>Registrarse</button>
-            <p>{username}</p>
-        </>
-    );
+  return (
+    <>
+      <label id="username">Nombre de usuario: </label>
+      <input
+        id="username"
+        type="text"
+        onChange={setUsernameHandler}
+        value={username}
+      ></input>
+      <button id="register-btn" type="submit" onClick={submitUsernameHandler}>
+        Registrarse
+      </button>
+      <p>{username}</p>
+    </>
+  );
 };
 
 export default Login;
